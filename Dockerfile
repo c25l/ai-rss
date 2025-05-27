3ac162db50497cbe043085172be73fd15068799e
Chris B <44374593+c25l@users.noreply.github.com>
2025-05-27 14:49:24 -0600

    Only what's needed to make it work.

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM condaforge/miniforge3
-WORKDIR /usr/src/app
-COPY requirements.txt .
-RUN conda install numpy pandas scipy scikit-learn networkx hdbscan
-RUN pip install -r requirements.txt
-COPY . .
-CMD ["python","airss.py"]
--- airss.py
+++ airss.py
@@ -3,12 +3,8 @@ from collections import defaultdict
 from datetime import datetime
 import numpy as np
 from datamodel import Article,Group
-from outputs import Outputs
 import feeds 
-from generator import Generator
-from modules import Weather, Stocks
 import os
-from cluster import Cluster
 import pickle
 import time
 import database
@@ -16,12 +12,36 @@ import smtplib
 from email.message import EmailMessage
 from markdown import markdown
 from datetime import datetime
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import requests
 
 def now(short = True):
     fstring = '%Y-%m-%d %H:%M:%S' if not short else '%Y-%m-%d'
     return datetime.now().strftime(fstring)
 def log(*inp):
     print(now(False), *inp)
+
+def embed(text, pq = 'p',norm=True):
+    payload = {
+        "model": "nomic-embed-text",
+        "prompt": ("passage: " if pq=='p' else "query: ") + text,
+    }
+    try:
+        response = requests.post("http://localhost:11434/api/embeddings", json=payload)
+        response.raise_for_status()
+        data = response.json()
+        vect = np.array(data.get("embedding",[]))
+        if norm:
+            vect /= np.linalg.norm(vect)
+        return vect
+    except Exception as e:
+        print(f"[ERROR] Model call failed: {e}")
+        return []
+
+###
+#  Clustering happens here.
+# ###    
 def make_gps(g, keyfunc):
     t_gps = defaultdict(list)
     misc = defaultdict(list)
@@ -42,10 +62,25 @@ def make_gps(g, keyfunc):
             t_gps[ii] = Group(text=", ".join([f"{ii}" for ii,jj in keys[:3]]) or xx[0].title, articles=xx)
 
     return t_gps, misc
-def cluster(gen, arts):
-    clust = Cluster(gen.embed, gen)
-    claims = [gen.embed(aa.title + "\n" + aa.summary) for aa in arts]
-    title_summary_clust = clust.cluster_vectors_kmeans(claims)
+
+def cluster_vectors_kmeans(embeddings):
+        # Optimal k-means clustering on embeddings
+        best_k = 2
+        best_score = -1
+        clusters_for_best_score = []
+        for k in range(4, len(embeddings)-1):
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            labels = kmeans.fit_predict(embeddings)
+            score = silhouette_score(embeddings, labels)
+            if score > best_score:
+                best_k = k
+                best_score = score
+                clusters_for_best_score = labels
+        return clusters_for_best_score
+
+def cluster(arts):
+    claims = [embed(aa.title + "\n" + aa.summary) for aa in arts]
+    title_summary_clust = cluster_vectors_kmeans(claims)
     tags = [
     'conflict', 'diplomacy', 'alliances', 'sanctions', 'elections', 'multipolarity',
     'Russia', 'Ukraine', 'NATO', 'Israel', 'Zionism', 'Antisemitism', 'Hamas', 'Gaza', 'Hezbollah',
@@ -69,7 +104,7 @@ def cluster(gen, arts):
     'misinformation', 'freedom', 'protest', 'repression', 'identity',
     'UNHCR', 'Human Rights Watch', 'Trump', 'President', 
     "music","concert","artist","album","linux","open source","python","programming","hardware","physics","quantum","nuclear",]
-    vtags = np.array([gen.embed(aa) for aa in tags])
+    vtags = np.array([embed(aa) for aa in tags])
     kwds = []
     tagvect = None
     for ii,xx in enumerate(claims):
@@ -78,7 +113,6 @@ def cluster(gen, arts):
         for uu in use:
             if sims[uu] < 0.65:
                 continue
-                use = use[:uu]
             if tagvect is None:
                 tagvect = np.zeros_like(vtags[uu]) 
             tagvect += vtags[uu] * sims[uu]
@@ -88,20 +122,12 @@ def cluster(gen, arts):
         arts[ii].keywords = use
         arts[ii].vector=xx
         kwds.append(use)
-    kwd_assigns = clust.cluster_vectors_kmeans([xx.vector for xx in arts])
-    # Step 1: Jaccard similarity on keyword sets
-    #claims = [bb.vector for bb in arts]
-    #print(claims)
-
-    # claims2 = clust.cluster_vectors_kmeans(claims)
-    #kwd_assigns = clust.cluster_jaccard_similarity(kwds)
-    # print(claims2, claims3, kwd_assigns)
-    #claims2 = clust.cluster_double_trouble(claims, kwds)
+    kwd_assigns = cluster_vectors_kmeans([xx.vector for xx in arts])
     print(title_summary_clust, kwd_assigns)
     cl = []
     clc = defaultdict(int)
-    for c2,kk in zip(title_summary_clust, kwd_assigns):#, claims3, kwd_assigns):
-        tcl = c2 +1000*kk#c3 * 1000+c2
+    for c2,kk in zip(title_summary_clust, kwd_assigns):
+        tcl = c2 +1000*kk
         cl.append(tcl)
         clc[tcl] += 1
     mhg = defaultdict(int)
@@ -111,26 +137,15 @@ def cluster(gen, arts):
     for aa,cc in zip(arts, cl):
         aa.cluster = cc
     return arts
-    
+###
+# Main control flow.
+###
 def main():
     today = now()
-
-    gen = Generator()
     articles = feeds.Feeds.fetch_articles(feeds.FEEDS)
 
     print(len(articles), "articles")
-    # for article in articles:
-    #     updated_article = gen.get_article_keywords(article)
-    #     updated_article.keywords = list(set((updated_article.keywords or [])))
-    #     updated_articles.append(updated_article)
-
-    updated_articles = cluster(gen, articles)#updated_articles)
-    # maxt = max([xx.published_at for xx in updated_articles]) 
-    # print(len(updated_articles), "updated articles")
-    # updated_articles = [aa for aa in updated_articles if aa.published_at > maxt - 60*60*24*7]
-    # print(len(updated_articles), "updated articles today.")
-    
-    #db = database.Database()
+    updated_articles = cluster(articles)
 
     g = defaultdict(list)
     for aa in updated_articles:
@@ -145,43 +160,8 @@ def main():
     t_gps3, misc3 = make_gps(misc2, lambda x: (x%1000)+500)
     t_gps.update(t_gps3)
     t_gps[-1] = Group(text="Misc.", articles=[xx[0] for _, xx in misc3.items()])
-    
-    print(t_gps)
-
-    #outgps= {g2[xx][0].articles[0].section: Group(text=g2[xx][0].articles[0].section,articles=g2[xx]) for xx in g2}
-    outgps=t_gps
-
-    
-
-    #  try:
-    #     if os.path.exists(f"{today}-annotated.pkl"):
-    #         with open(f"{today}-annotated.pkl", "r") as f:
-    #             print("Loading previous annotations")
-    #             updated_articles = pickle.load(f)
-    #     else:
-    #         print("No previous annotations")
-    # except:
-    #     print("Error loading previous annotations, annotating new articles")
-    #     for article in articles:
-    #         updated_article = gen.get_article_keywords(article)
-    #         updated_article.keywords = list(set((updated_article.keywords or [])))
-    #         updated_articles.append(updated_article)
-
-    # try:
-    #     with open(f"{today}-annotated.pkl", "w") as f:
-    #         pickle.dump(updated_articles, f)
-    # except:
-    #     pass
-    # print(updated_articles)
-    # print(t_gps)
-    # for group in t_gps:
-    #     # Add group to the database
-    #     db_group = Group([]
-    #         text=group.text,
-    #         created_at=datetime.now()
-    #     )
-
-    text = "\n".join([tgp.out() for _, tgp in outgps.items()])
+    print("group ct: ", len(t_gps))
+    text = "\n".join([tgp.out() for _, tgp in t_gps.items()])
     sender = "christopherpbonnell@icloud.com"
     receiver = "christopherpbonnell@gmail.com"
     password="vqxh-oqrp-wjln-eagl"
@@ -196,10 +176,6 @@ def main():
         server.login(msg['From'], password)
         server.send_message(msg)
 
-    # Outputs.md_fileout(
-    #     "\n".join(tmp + ["# Articles"] + [tgp.out() for _, tgp in outgps.items()]),
-    #     "./"
-    # )
 
 if __name__ == "__main__":
     main()
